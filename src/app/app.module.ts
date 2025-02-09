3b3538a1250c96f3f93c4df8a1f79bf7383a541e
plcrisp <pedrolcrisp@gmail.com>
2025-02-09 16:29:28 -0300

    🚀 add Tailwind CSS configuration and integrate it into the login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,15 @@ import { LoginComponent } from './components/login/login.component';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { environment } from '../environments/environment';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { RecuperarSenhaComponent } from './components/recuperar-senha/recuperar-senha.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CadastroComponent,
+    RecuperarSenhaComponent
   ],
   imports: [
     BrowserModule,
