9a3dabdfa24991f9e11a463bb8f6df31daf5461f
plcrisp <pedrolcrisp@gmail.com>
2025-02-09 16:49:02 -0300

    📄  add login base code

---
+++ base-codes/login/login.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  email: string ='';
+  password: string = '';
+  rememberMe: boolean = false;
+
+}
